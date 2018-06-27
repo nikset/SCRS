ce33cdd86dbf0fcbc42b7d8e35f3fea896751702
Nicola Nicola <nikset@hotmail.it>
2018-06-27 22:16:01 +0200

    Captcha Autentication Project

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/base_domain/work/springmvchelloworld/jsp_servlet/_pages/__welcome.java
@@ -0,0 +1,146 @@
+package jsp_servlet._pages;
+
+import java.lang.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class __welcome extends  weblogic.servlet.jsp.JspBase  implements weblogic.servlet.jsp.StaleIndicator {
+
+    private static void _releaseTags(javax.servlet.jsp.PageContext pageContext, javax.servlet.jsp.tagext.JspTag t) {
+        while (t != null) {
+            weblogic.servlet.jsp.DependencyInjectionHelper.preDestroy(pageContext, t);
+            if(t instanceof javax.servlet.jsp.tagext.Tag) {
+                javax.servlet.jsp.tagext.Tag tmp = (javax.servlet.jsp.tagext.Tag)t;
+                t = ((javax.servlet.jsp.tagext.Tag) t).getParent();
+                try {
+                    tmp.release();
+                } catch(java.lang.Exception ignore) {}
+            }
+            else {
+                t = ((javax.servlet.jsp.tagext.SimpleTag)t).getParent();
+            }
+        }
+    }
+
+    public boolean _isStale(){
+        boolean _stale = _staticIsStale((weblogic.servlet.jsp.StaleChecker) getServletConfig().getServletContext());
+        return _stale;
+    }
+
+    public static boolean _staticIsStale(weblogic.servlet.jsp.StaleChecker sci) {
+        if (sci.isResourceStale("/pages/welcome.jsp", 1529770685064L ,"10.3.6.0","Europe/Berlin")) return true;
+        return false;
+    }
+
+    private static boolean _WL_ENCODED_BYTES_OK = true;
+    private static final java.lang.String _WL_ORIGINAL_ENCODING = "ISO-8859-1".intern();
+
+    private static byte[] _getBytes(java.lang.String block){
+        try {
+            return block.getBytes(_WL_ORIGINAL_ENCODING);
+        } catch (java.io.UnsupportedEncodingException u){
+            _WL_ENCODED_BYTES_OK = false;
+        }
+        return null;
+    }
+
+    private final static java.lang.String  _wl_block0 ="<!DOCTYPE HTML>\r\n";
+    private final static byte[]  _wl_block0Bytes = _getBytes( _wl_block0 );
+
+    private final static java.lang.String  _wl_block1 ="\r\n";
+    private final static byte[]  _wl_block1Bytes = _getBytes( _wl_block1 );
+
+    private final static java.lang.String  _wl_block2 ="\r\n<html>\r\n<head>\r\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">\r\n    <title>Sping MVC Redirection Application</title>\r\n</head>\r\n<body>\r\n    <h2>Spring Page Redirection Example</h2>\r\n    <div id=\"welcomeTextDiv\">\r\n        <span id=\"btnText\" style=\"padding-left: 24px;\">!! Click Below Button To Redirect The Result To The New Page !!</span>\r\n        <div id=\"redirectBtnTable\" style=\"padding: 23px 0px 0px 35px;\">\r\n        \r\n        \r\n        \r\n        \r\n            ";
+    private final static byte[]  _wl_block2Bytes = _getBytes( _wl_block2 );
+
+    private final static java.lang.String  _wl_block3 ="\r\n            <input name=\"userName\" type=\"text\" value=\"qui il tuo testo\" size=\"40\" maxlength=\"200\" />\r\n             <input name=\"password\" type=\"password\" value=\"qui il tuo testo\" size=\"40\" maxlength=\"200\" />\r\n            \r\n                <table>\r\n                    <tbody>\r\n                        <tr>\r\n                            <td>\r\n                                <input id=\"redirectBtn\" type=\"submit\" value=\"Redirect Page\" />\r\n                            </td>\r\n                        </tr>\r\n                    </tbody>\r\n                </table>\r\n            ";
+    private final static byte[]  _wl_block3Bytes = _getBytes( _wl_block3 );
+
+    private final static java.lang.String  _wl_block4 ="\r\n        </div>\r\n    </div>\r\n</body>\r\n</html>\r\n";
+    private final static byte[]  _wl_block4Bytes = _getBytes( _wl_block4 );
+
+    static private weblogic.jsp.internal.jsp.JspFunctionMapper _jspx_fnmap = weblogic.jsp.internal.jsp.JspFunctionMapper.getInstance();
+
+    public void _jspService(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) 
+    throws javax.servlet.ServletException, java.io.IOException {
+
+        javax.servlet.ServletConfig config = getServletConfig();
+        javax.servlet.ServletContext application = config.getServletContext();
+        javax.servlet.jsp.tagext.JspTag _activeTag = null;
+        java.lang.Object page = this;
+        javax.servlet.jsp.PageContext pageContext = javax.servlet.jsp.JspFactory.getDefaultFactory().getPageContext(this, request, response, null, true , 8192 , true );
+        response.setHeader("Content-Type", "text/html; charset=ISO-8859-1");
+        javax.servlet.jsp.JspWriter out = pageContext.getOut();
+        weblogic.servlet.jsp.ByteWriter _bw = (weblogic.servlet.jsp.ByteWriter)out;
+        _bw.setInitCharacterEncoding(_WL_ORIGINAL_ENCODING, _WL_ENCODED_BYTES_OK);
+        javax.servlet.jsp.JspWriter _originalOut = out;
+        javax.servlet.http.HttpSession session = request.getSession( true );
+        try {;
+            response.setContentType("text/html; charset=ISO-8859-1");
+            _bw.write(_wl_block0Bytes, _wl_block0);
+            _bw.write(_wl_block1Bytes, _wl_block1);
+            _bw.write(_wl_block2Bytes, _wl_block2);
+
+            if (_jsp__tag0(request, response, pageContext, _activeTag, null))
+             return;
+            _bw.write(_wl_block4Bytes, _wl_block4);
+        } catch (java.lang.Throwable __ee){
+            if(!(__ee instanceof javax.servlet.jsp.SkipPageException)) {
+                while ((out != null) && (out != _originalOut)) out = pageContext.popBody(); 
+                _releaseTags(pageContext, _activeTag);
+                pageContext.handlePageException(__ee);
+            }
+        }
+    }
+
+    private boolean _jsp__tag0(javax.servlet.ServletRequest request, javax.servlet.ServletResponse response, javax.servlet.jsp.PageContext pageContext, javax.servlet.jsp.tagext.JspTag activeTag, javax.servlet.jsp.tagext.JspTag parent) throws java.lang.Throwable
+    {
+        javax.servlet.jsp.tagext.JspTag _activeTag = activeTag;
+        javax.servlet.jsp.JspWriter out = pageContext.getOut();
+        weblogic.servlet.jsp.ByteWriter _bw = (weblogic.servlet.jsp.ByteWriter) out;
+         org.springframework.web.servlet.tags.form.FormTag __tag0 = null ;
+        int __result__tag0 = 0 ;
+        boolean _skipPage__tag0= false;
+
+        if (__tag0 == null ){
+            __tag0 = new  org.springframework.web.servlet.tags.form.FormTag ();
+            weblogic.servlet.jsp.DependencyInjectionHelper.inject(pageContext, __tag0);
+        }
+        __tag0.setPageContext(pageContext);
+        __tag0.setParent(null);
+        __tag0.setId(( java.lang.String ) weblogic.jsp.internal.jsp.utils.JspRuntimeUtils.convertType("redirectionForm", java.lang.String .class,"id"));
+        __tag0.setAction(( java.lang.String ) weblogic.jsp.internal.jsp.utils.JspRuntimeUtils.convertType("redirect_page", java.lang.String .class,"action"));
+        __tag0.setMethod(( java.lang.String ) weblogic.jsp.internal.jsp.utils.JspRuntimeUtils.convertType("GET", java.lang.String .class,"method"));
+        try {_activeTag=__tag0;
+            __result__tag0 = __tag0.doStartTag();
+
+            if (__result__tag0!= javax.servlet.jsp.tagext.Tag.SKIP_BODY){
+                if (__result__tag0== javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_BUFFERED) {
+                     throw  new  javax.servlet.jsp.JspTagException("Since tag class org.springframework.web.servlet.tags.form.FormTag does not implement BodyTag, it cannot return BodyTag.EVAL_BODY_BUFFERED");
+                }
+                do {
+                    _bw.write(_wl_block3Bytes, _wl_block3);
+                } while (__tag0.doAfterBody()== javax.servlet.jsp.tagext.IterationTag.EVAL_BODY_AGAIN);
+            }
+            if (__tag0.doEndTag()== javax.servlet.jsp.tagext.Tag.SKIP_PAGE){
+                _activeTag = null;
+                _skipPage__tag0 = true;
+                return true;
+            }
+            _activeTag=__tag0.getParent();
+            _skipPage__tag0 = false;
+        } catch (java.lang.Throwable __t){
+            __tag0.doCatch(__t);
+        } finally {
+            __tag0.doFinally();
+            if (!_skipPage__tag0){
+                weblogic.servlet.jsp.DependencyInjectionHelper.preDestroy(pageContext, __tag0);
+                __tag0.release();
+            }else{
+                _releaseTags(pageContext, __tag0);
+            }
+        }
+        return false;
+    }
+}
